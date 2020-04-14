34807c5583316f4ddea71d7d169793f1480639bb
Krishna Sheth <krishna.sheth@ibm.com>
2020-04-14 18:46:41 -0400

    Add tests for hdx_acap

---
+++ tests/data_sources/hdx_acap/test___init__.py
@@ -0,0 +1,24 @@
+from unittest import TestCase, skip
+
+from task_geo.data_sources.hdx_acap.hdx_acap_formatter import hdx_acap_formatter
+from task_geo.data_sources.hdx_acap.hdx_acap_connector import hdx_acap_connector
+from task_geo.testing import check_dataset_format
+
+
+class TestHdxApi(TestCase):
+
+    @skip
+    def test_validate_format_raw_output(self):
+        """Validate datasource output.
+
+        This tests is skipped intentionally on the main suite due to it taking to long.
+        Is kept for developing porpouses.
+        """
+        # Setup
+        raw = hdx_acap_connector()
+
+        # Run
+        data = hdx_acap_formatter(raw)
+
+        # Check.
+        check_dataset_format(data)
---
+++ tests/data_sources/hdx_acap/test_hdx_acap_formatter.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+import pandas as pd
+
+from task_geo.data_sources.hdx_acap.hdx_acap_formatter import hdx_acap_formatter
+from task_geo.testing import check_dataset_format
+
+
+class TestHdxApi(TestCase):
+
+    def test_validate_formatter(self):
+        """Validate Output of datasource
+        """
+        # Setup
+        raw = pd.read_csv('../../fixtures/hdx_acap_fixture.csv')
+
+        # Run
+        data = hdx_acap_formatter(raw)
+
+        # Check.
+        check_dataset_format(data)
