7e74d3c98a1eda028ff2e8d36005c73524c9cb2e
Krishna Sheth <krishna.sheth@ibm.com>
2020-04-14 18:49:37 -0400

    Add tests for hdx_oxfd

---
+++ tests/data_sources/hdx_oxfd/test___init__.py
@@ -0,0 +1,28 @@
+from unittest import TestCase, skip
+
+from task_geo.data_sources.hdx_oxfd.hdx_oxfd_connector import hdx_oxfd_connector
+from task_geo.data_sources.hdx_oxfd.hdx_oxfd_formatter import hdx_oxfd_formatter
+from task_geo.testing import check_dataset_format
+from datetime import datetime
+
+
+class TestHdxApi(TestCase):
+
+    @skip
+    def test_validate_format_raw_output(self):
+        """Validate datasource output.
+
+        This tests is skipped intentionally on the main suite due to it taking to long.
+        Is kept for developing porpouses.
+        """
+        # Setup
+        start_date = datetime(2020, 3, 1).date().isoformat()
+        end_date = datetime(2020, 3, 15).date().isoformat()
+        countries = ['US']
+        raw = hdx_oxfd_connector()
+
+        # Run
+        data = hdx_oxfd_formatter(raw, start_date, end_date, countries)
+
+        # Check.
+        check_dataset_format(data)
---
+++ tests/data_sources/hdx_oxfd/test_hdx_oxfd_formatter.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+import pandas as pd
+from datetime import datetime
+
+from task_geo.testing import check_dataset_format
+from task_geo.data_sources.hdx_oxfd.hdx_oxfd_formatter import hdx_oxfd_formatter
+
+
+class TestHdxApi(TestCase):
+
+    def test_validate_formatter(self):
+        """Validate datasource output.
+        """
+        # Setup
+        start_date = datetime(2020, 1, 1).date().isoformat()
+        end_date = datetime(2020, 2, 15).date().isoformat()
+        countries = ["AW"]
+        raw = pd.read_csv('../../fixtures/hdx_oxfd_fixture.csv')
+
+        # Run
+        data = hdx_oxfd_formatter(raw, start_date, end_date, countries)
+
+        # Check.
+        check_dataset_format(data)
